bed04b5f9093f0cd9d9a6e77623f5cef748da59f
Didier E. TAVARES <didier.tavares@gmail.com>
2025-05-26 17:33:00 -0300

    concluded 26.05

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 const h2 = document.querySelector("h2")
 const p = document.querySelector('p')
 const body = document.querySelector('body')
-body.style.background = '#ffffff'
+body.style.background = 'WHITE'
 p.innerText = "WHITE"
-p.style.color = '#ffffff'
+p.style.color = 'WHITE'
 let indexSelected
-let colorRefSelected
 let colorNameSelected
 
-const colorOptions = [
-    {name:"WHITE", ref: '#ffffff'},
-    {name:"BLUE", ref: '#0000ff'},
-    {name:"GREEN", ref: '#00ff00'},
-    {name:"GREY", ref: '#808080'},
-    {name:"BLACK", ref: '#000000'},
-    {name:"PINK", ref: '#ffc0cb'},
-    {name:"ORANGE", ref: '#ffa500'},
-    {name:"YELLOW", ref: '#ffff00'},
-    {name:"BEIGE", ref: '#f5f5dc'},
-    {name:"RED", ref: '#ff0000'}
-]
+const colorOptions = ["WHITE", "BLUE", "GREEN", "GREY", "BLACK", "PINK", "ORANGE", "YELLOW", "BEIGE", "RED"]
+
 console.log(colorOptions)
 console.log(colorOptions.length)
 
 function randomColor()  {
   indexSelected = Math.floor(Math.random() * colorOptions.length)
   console.log(indexSelected)
-  colorRefSelected = colorOptions[indexSelected].ref
-  console.log(colorRefSelected)
-  colorNameSelected = colorOptions[indexSelected].name
+  colorNameSelected = colorOptions[indexSelected]
   console.log(colorNameSelected)
   display()
 }
 
+function randomColor2() {
+  let r = Math.floor(Math.random() * (255+1))
+  let g = Math.floor(Math.random() * (255+1))
+  let b = Math.floor(Math.random() * (255+1))
+  display2(r, g, b)
+
+}
+
 function display()  {
-  body.style.background = colorRefSelected
+  body.style.background = colorNameSelected
   p.innerText = colorNameSelected
-  p.style.color = colorRefSelected
+  p.style.color = colorNameSelected
+}
+function display2(r, g, b) {
+  body.style.background = `rgb(${r}, ${g}, ${b})`
+  p.innerText = `rgb(${r}, ${g}, ${b})`
+  p.style.color = `rgb(${r}, ${g}, ${b})`
+
 }
