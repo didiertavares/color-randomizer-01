f894ba73f01f92512ff7b650b625a93ed90bd1fa
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2025-05-26 12:01:56 -0300

    MàJ 21.05

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-const p = document.querySelector("p")
-const Span = document.querySelector("span")
+const h2 = document.querySelector("h2")
+const p = document.querySelector('p')
+const body = document.querySelector('body')
+body.style.background = '#ffffff'
+p.innerText = "WHITE"
+p.style.color = '#ffffff'
+let indexSelected
+let colorRefSelected
+let colorNameSelected
 
 const colorOptions = [
-    {name:"white", ref: #ffffff}
-    {name:"blue", ref: #0000ff}
-    {name:"green", ref: #00ff00}
-    {name:"grey", ref: #808080}
-    {name:"black", ref: #000000}
-    {name:"pink", ref: #ffc0cb}
-    {name:"orange", ref: #ffa500}
-    {name:"yellow", ref: #ffff00}
-    {name:"beige", ref: #f5f5dc}
+    {name:"WHITE", ref: '#ffffff'},
+    {name:"BLUE", ref: '#0000ff'},
+    {name:"GREEN", ref: '#00ff00'},
+    {name:"GREY", ref: '#808080'},
+    {name:"BLACK", ref: '#000000'},
+    {name:"PINK", ref: '#ffc0cb'},
+    {name:"ORANGE", ref: '#ffa500'},
+    {name:"YELLOW", ref: '#ffff00'},
+    {name:"BEIGE", ref: '#f5f5dc'},
+    {name:"RED", ref: '#ff0000'}
 ]
-
-// const oneColor = {name:"white", ref: #ffffff}
-// console.log(oneColor["name"])
+console.log(colorOptions)
+console.log(colorOptions.length)
 
 function randomColor()  {
-  const randomIndex = Math.floor(Math.random() * colorOptions.length)
-  colorName = randomIndex[name]
-  console.log(colorName)
-  Span.innerText = colorOptions[randomIndex]
+  indexSelected = Math.floor(Math.random() * colorOptions.length)
+  console.log(indexSelected)
+  colorRefSelected = colorOptions[indexSelected].ref
+  console.log(colorRefSelected)
+  colorNameSelected = colorOptions[indexSelected].name
+  console.log(colorNameSelected)
+  display()
+}
+
+function display()  {
+  body.style.background = colorRefSelected
+  p.innerText = colorNameSelected
+  p.style.color = colorRefSelected
 }
